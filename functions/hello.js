70595d4b9af402af7352363b83b844305da491f9
Anda Popovici <anda.popovici@freeagent.com>
2020-10-19 17:06:55 +0100

    Fingers crpssed

--- functions/hello.js
+++ functions/hello.js
@@ -1,12 +1,17 @@
+import sha256 from 'crypto-js/sha256';
+
 exports.handler = function(event, context, callback) {
-  const { API_TOKEN } = process.env
+  const { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET} = process.env
+
+  const timestamp = Date.now();
+  const signature = `cloud_name=${CLOUDINARY_CLOUD_NAME}}&timestamp=${timestamp}&username=website@freeagent.com${CLOUDINARY_API_SECRET}`
 
   callback(null, {
     headers: {
-      "Access-Control-Allow-Origin" : "open-media-library-manually--cms-cloudinary-demo.netlify.app",
-      "Access-Control-Allow-Methods": "GET"
+      "Access-Control-Allow-Origin" :"open-media-library-manually--cms-cloudinary-demo.netlify.app",
+      "Access-Control-Allow-Methods":"GET"
     },
     statusCode: 200,
-    body: "Hello, World " + API_TOKEN
+    body: sha256(signature)
   });
   };
