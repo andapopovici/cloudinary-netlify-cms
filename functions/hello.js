69e655c1cace258335e4f76995a4843999609fae
Anda Popovici <anda.popovici@freeagent.com>
2020-10-19 16:26:22 +0100

    read netlify env var

--- functions/hello.js
+++ functions/hello.js
@@ -1,10 +1,12 @@
 exports.handler = function(event, context, callback) {
+  const { API_TOKEN } = process.env
+
   callback(null, {
     headers: {
-      "Access-Control-Allow-Origin" : "*",
+      "Access-Control-Allow-Origin" : "open-media-library-manually--cms-cloudinary-demo.netlify.app",
       "Access-Control-Allow-Methods": "GET"
     },
     statusCode: 200,
-    body: "Hello, World"
+    body: "Hello, World " + API_TOKEN
   });
   };
