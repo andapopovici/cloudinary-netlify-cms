38c818c774cbf2243f28d82ef2e2ad2ffe4b46c3
Anda Popovici <anda.popovici@freeagent.com>
2020-10-19 17:13:44 +0100

    Use require

--- functions/hello.js
+++ functions/hello.js
@@ -1,8 +1,7 @@
-import sha256 from 'crypto-js/sha256';
-
 exports.handler = function(event, context, callback) {
-  const { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET} = process.env
+  const sha256 = require("crypto-js/sha256");
 
+  const { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET} = process.env
   const timestamp = Date.now();
   const signature = `cloud_name=${CLOUDINARY_CLOUD_NAME}}&timestamp=${timestamp}&username=website@freeagent.com${CLOUDINARY_API_SECRET}`
 
