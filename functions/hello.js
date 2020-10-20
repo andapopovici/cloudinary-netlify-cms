76102b76f20e77aae537c3e2b6bc2bd5ac000ffd
Anda Popovici <andapopovici@users.noreply.github.com>
2020-10-20 16:10:56 +0100

    Seamlessly log into Cloudinary when opening the media library

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,19 @@
 exports.handler = function(event, context, callback) {
-    callback(null, {
-      statusCode: 200,
-      body: "Hello, World"
-    });
-  };
+  const sha256 = require("crypto-js/sha256");
+
+  const { CLOUDINARY_API_SECRET} = process.env
+  const timestamp = Date.now();
+  const signature = `cloud_name=drzfqrj57&timestamp=${timestamp}&username=website@freeagent.com${CLOUDINARY_API_SECRET}`
+
+  callback(null, {
+    headers: {
+      "Access-Control-Allow-Origin" :"cms-cloudinary-demo.netlify.app",
+      "Access-Control-Allow-Methods":"GET"
+    },
+    statusCode: 200,
+    body: JSON.stringify({
+      signature: sha256(signature).toString(),
+      timestamp: timestamp
+    })
+  });
+};
